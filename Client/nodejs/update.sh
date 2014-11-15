e7d4c1fc89d18fb3a73e8119331d6104b1a5c102
Lucas Teske <lucas@teske.com.br>
2014-11-15 18:49:00 -0200

    Changed update script to wait for executing next

--- Client/nodejs/update.sh
+++ Client/nodejs/update.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+LOCKFILE=/tmp/smartmon
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`
+then
+        echo "Já rodando!"
+        exit
+fi
+
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
 cd /root/ac
 node smart_monitor.js 2> lastlog 1> lastlog
 
